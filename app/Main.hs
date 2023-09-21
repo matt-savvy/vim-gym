ad98b5bd47ada530c8b9ce5786fe61479d289a91
Matt Savoia <matt@1-800-rad-dude.com>
2023-09-21 15:37:34 -0400

    Update Main, add Review

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,18 @@
 module Main (main) where
 import System.Environment (getArgs)
 import System.IO (withFile, IOMode(..), hGetContents)
+import System.Process
 
-data Command = AddDrill { path :: FilePath } deriving (Show)
+data Command = AddDrill { path :: FilePath } | Review deriving (Show)
 
 handleArgs :: [String] -> Command
 handleArgs ["add", filePath] = AddDrill { path = filePath }
+handleArgs ["review"] = Review
 handleArgs _args = undefined
 
 handleCommand :: Command -> IO ()
 handleCommand (AddDrill filePath) = copyFile filePath
+handleCommand Review = review
 
 copyFile :: FilePath -> IO ()
 copyFile filePath = do
@@ -19,8 +22,24 @@ copyFile filePath = do
               contents <- hGetContents handle
               writeFile newFilePath contents
 
+getDrill :: IO (Maybe FilePath)
+getDrill = return $ Just "tmp_lorem.txt"
+
+review :: IO ()
+review = do
+    drill <- getDrill
+    case drill of
+        Just filePath -> do
+            _ <- runCommand $ vimCommand filePath
+            print ""
+        Nothing -> putStrLn "all done"
+
+vimCommand :: FilePath -> String
+vimCommand filePath = "/home/mattsavoia/.nix-profile/bin/vim" <> " " <> filePath
+
 main :: IO ()
 main = do
     args <- getArgs
     let command = handleArgs args
     handleCommand command
+    print ""
