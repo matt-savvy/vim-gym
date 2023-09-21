54c23736ec0eb5c9ee7d22463aebf4e65b7674ae
Matt Savoia <matt@1-800-rad-dude.com>
2023-09-21 10:28:13 -0400

    Update Main, add handleArgs, handleCommand

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,26 @@
 module Main (main) where
+import System.Environment (getArgs)
+import System.IO (withFile, IOMode(..), hGetContents)
+
+data Command = AddDrill { path :: FilePath } deriving (Show)
+
+handleArgs :: [String] -> Command
+handleArgs ["add", filePath] = AddDrill { path = filePath }
+handleArgs _args = undefined
+
+handleCommand :: Command -> IO ()
+handleCommand (AddDrill filePath) = copyFile filePath
+
+copyFile :: FilePath -> IO ()
+copyFile filePath = do
+    withFile filePath ReadMode processFile
+    where newFilePath = "tmp_" <> filePath
+          processFile handle = do
+              contents <- hGetContents handle
+              writeFile newFilePath contents
 
 main :: IO ()
-main = putStrLn ""
+main = do
+    args <- getArgs
+    let command = handleArgs args
+    handleCommand command
