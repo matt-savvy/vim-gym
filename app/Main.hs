80ef369090a3d3ee00d325f63e0fa9517fbaa8f9
Matt Savoia <matt@1-800-rad-dude.com>
2023-09-22 15:27:55 -0400

    Update Main.hs, add MVP

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,22 @@
 module Main (main) where
+import qualified SM2
 import System.Environment (getArgs)
 import System.IO (withFile, IOMode(..), hGetContents)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as TIO
 import System.Process
-import Database.SQLite.Simple (Connection, withConnection, Query, execute)
-import Database.SQLite.Simple.ToRow (toRow, ToRow)
-import GHC.Generics (Generic)
-import Data.Time (Day(..))
+import Database.SQLite.Simple
+import Data.Time (Day(..), getCurrentTime, utctDay)
 
 data Command = AddDrill { path :: FilePath } | Review deriving (Show)
 
 data Drill = Drill { drillId :: Int, fileName :: FilePath, body :: Text.Text } deriving (Show)
+
 instance ToRow Drill where
     toRow (Drill _id fileName body) = toRow (fileName, body)
 
-data Grade = Grade { gradeId :: Int, drill :: Drill, streak :: Int, score :: Float, interval :: Int, lastReviewed :: Day }
+
+data Grade = Grade { gradeId :: Int, drill :: Drill, streak :: Int, score :: Float, interval :: Int, lastReviewed :: Day } deriving Show
 
 instance ToRow Grade where
     toRow (Grade _id drill streak score interval lastReviewed) = toRow ( (drillId drill), streak, score, interval, lastReviewed)
@@ -37,7 +38,7 @@ handleArgs _args = undefined
 
 handleCommand :: Command -> Connection -> IO ()
 handleCommand (AddDrill filePath) conn = drillFromFile filePath conn
-handleCommand Review _conn = review
+handleCommand Review conn = review conn
 
 drillFromFile :: FilePath -> Connection -> IO ()
 drillFromFile filePath conn = do
@@ -45,8 +46,9 @@ drillFromFile filePath conn = do
     where processFile handle = do
               contents <- TIO.hGetContents handle
               let drill = Drill { drillId = 0, fileName = filePath, body = contents }
-              let grade = initGrade drill
               execute conn insertDrillQuery drill
+              rowId <- lastInsertRowId conn
+              let grade = initGrade (drill {drillId = (fromIntegral rowId)})
               execute conn insertGradeQuery grade
 
 insertDrillQuery :: Query
@@ -55,18 +57,67 @@ insertDrillQuery = "INSERT INTO drills (filename, body) VALUES (?, ?)"
 insertGradeQuery :: Query
 insertGradeQuery = "INSERT INTO grades (drill_id, streak, score, interval, last_reviewed) VALUES (?, ?, ?, ?, ?)"
 
+updateGradeQuery :: Query
+updateGradeQuery = "UPDATE grades SET streak = ?, score = ?, interval = ?, last_reviewed = ? WHERE id = ?"
 
-getDrill :: IO (Maybe FilePath)
-getDrill = return $ Just "tmp_lorem.txt"
+getDueQuery :: Query
+getDueQuery = "SELECT grades.id, drill_id, streak, score, filename, body, interval FROM grades JOIN drills ON grades.drill_id = drills.id WHERE datetime(last_reviewed, '+' || interval || ' day') <= datetime('now') LIMIT 1;"
 
-review :: IO ()
-review = do
-    drill <- getDrill
-    case drill of
-        Just filePath -> do
-            _ <- runCommand $ vimCommand filePath
-            print ""
-        Nothing -> putStrLn "all done"
+getDrillsQuery :: Query
+getDrillsQuery = "SELECT * FROM drills WHERE id IN (?) LIMIT 1;"
+
+getGrades :: Connection -> IO [Grade]
+getGrades conn = do
+    grades <- query_ conn getDueQuery
+    return $ map toGrade grades
+
+toGrade :: (Int, Int, Int, Float, FilePath, Text.Text, Int) -> Grade
+toGrade (gradeId, drillId, streak, score, filename, body, interval) =
+    Grade { gradeId = gradeId
+          , streak = streak
+          , score = score
+          , drill = Drill { drillId = drillId, fileName = filename, body = body }
+          , interval = interval
+          , lastReviewed = (ModifiedJulianDay 0)
+          }
+
+
+review :: Connection -> IO ()
+review conn = do
+    grades <- getGrades conn
+    case grades of
+        [] -> putStrLn "all done"
+        (grade : _rest) -> do
+            let drill' = drill grade
+            let filename = fileName drill'
+            callCommand $ vimCommand filename
+            newScore <- getScore
+            currentDay <- getCurrentDay
+            let newGrade' = applySM2Grade grade (SM2.applyScore newScore (toSM2Grade grade))
+            -- newGrade' should have current day + (interval newGrade)
+            let newGrade = newGrade' { lastReviewed = currentDay }
+            execute conn updateGradeQuery (streak newGrade, score newGrade, interval newGrade, lastReviewed newGrade, gradeId newGrade)
+
+
+toSM2Grade :: Grade -> SM2.Grade
+toSM2Grade grade =
+    SM2.Grade (streak grade) (score grade) (interval grade)
+
+applySM2Grade :: Grade -> SM2.Grade -> Grade
+applySM2Grade grade (SM2.Grade streak score interval) =
+    grade { streak = streak, score = score, interval = interval}
+
+getScore :: IO Float
+getScore = do
+    putStrLn "how did you do? score from 0 - 5"
+    line <- getLine
+    let newScore = read line
+    return newScore
+
+getCurrentDay :: IO Day
+getCurrentDay = do
+    utcTime <- getCurrentTime
+    return $ utctDay utcTime
 
 vimCommand :: FilePath -> String
 vimCommand filePath = "/home/mattsavoia/.nix-profile/bin/vim" <> " " <> filePath
