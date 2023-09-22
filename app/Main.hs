f496db7997b94677dec838483170e5015a95ee82
Matt Savoia <matt@1-800-rad-dude.com>
2023-09-22 13:41:43 -0400

    Update Main, add drillFromFile

--- app/Main.hs
+++ app/Main.hs
@@ -2,28 +2,39 @@ module Main (main) where
 import System.Environment (getArgs)
 import System.IO (withFile, IOMode(..), hGetContents)
 import qualified Data.Text as Text
+import qualified Data.Text.IO as TIO
 import System.Process
+import Database.SQLite.Simple (Connection, withConnection, Query, execute)
+import Database.SQLite.Simple.ToRow (toRow, ToRow)
+import GHC.Generics (Generic)
 
 data Command = AddDrill { path :: FilePath } | Review deriving (Show)
 
 data Drill = Drill { fileName :: FilePath, body :: Text.Text } deriving (Show)
+
+instance ToRow Drill where
+    toRow (Drill fileName body) = toRow (fileName, body)
+
 
 handleArgs :: [String] -> Command
 handleArgs ["add", filePath] = AddDrill { path = filePath }
 handleArgs ["review"] = Review
 handleArgs _args = undefined
 
-handleCommand :: Command -> IO ()
-handleCommand (AddDrill filePath) = copyFile filePath
-handleCommand Review = review
+handleCommand :: Command -> Connection -> IO ()
+handleCommand (AddDrill filePath) conn = drillFromFile filePath conn
+handleCommand Review _conn = review
 
-copyFile :: FilePath -> IO ()
-copyFile filePath = do
+drillFromFile :: FilePath -> Connection -> IO ()
+drillFromFile filePath conn = do
     withFile filePath ReadMode processFile
-    where newFilePath = "tmp_" <> filePath
-          processFile handle = do
-              contents <- hGetContents handle
-              writeFile newFilePath contents
+    where processFile handle = do
+              contents <- TIO.hGetContents handle
+              let drill = Drill { fileName = filePath, body = contents }
+              execute conn insertDrillQuery drill
+
+insertDrillQuery :: Query
+insertDrillQuery = "INSERT INTO drills (filename, body) VALUES (?, ?)"
 
 getDrill :: IO (Maybe FilePath)
 getDrill = return $ Just "tmp_lorem.txt"
@@ -40,9 +51,11 @@ review = do
 vimCommand :: FilePath -> String
 vimCommand filePath = "/home/mattsavoia/.nix-profile/bin/vim" <> " " <> filePath
 
+
 main :: IO ()
 main = do
     args <- getArgs
     let command = handleArgs args
-    handleCommand command
-    print ""
+    withConnection "vim-gym.db" $ \conn -> do
+        handleCommand command conn
+        print ""
