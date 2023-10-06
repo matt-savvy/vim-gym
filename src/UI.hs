b8699b3458de3be287808ce7a2c62474bcfab072
Matt Savoia <matt@1-800-rad-dude.com>
2023-10-05 23:05:20 -0400

    Add UI.getContinue

---
+++ src/UI.hs
@@ -0,0 +1,23 @@
+module UI (getContinue) where
+import Data.Text (Text, toLower)
+import qualified Data.Text.IO as TIO
+
+getContinue :: IO Bool
+getContinue = do
+    putStrLn "continue? y/n"
+    input <- TIO.getLine
+    let continue = parseContinue input
+    case continue of
+        Nothing -> do
+            putStrLn "invalid input"
+            getContinue
+        Just continue' -> return continue'
+
+parseContinue :: Text -> Maybe Bool
+parseContinue input = parseContinue' cleanInput
+    where cleanInput = toLower input
+          parseContinue' "y" = Just True
+          parseContinue' "n" = Just False
+          parseContinue' _ = Nothing
+
+
