7381ee3d3fd7d7e622ea236e13e8696e0a958078
Matt Savoia <matt@1-800-rad-dude.com>
2023-10-07 14:42:53 -0400

    Update UI, move getScore

--- app/Main.hs
+++ app/Main.hs
@@ -6,12 +6,9 @@ import System.Environment (getArgs)
 import System.IO (withFile, IOMode(..), hGetContents)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as TIO
-import Text.Read (readMaybe)
 import System.Process
 import Database.SQLite.Simple
 import Data.Time (Day(..), getCurrentTime, utctDay)
-
-data Command = AddDrill { path :: FilePath } | Review | Status deriving (Show)
 
 data Drill = Drill { drillId :: Int, fileName :: FilePath, body :: Text.Text } deriving (Show)
 
@@ -34,16 +31,10 @@ initGrade drill = Grade
     , lastReviewed = (ModifiedJulianDay 0)
     }
 
-handleArgs :: [String] -> Command
-handleArgs ["add", filePath] = AddDrill { path = filePath }
-handleArgs ["review"] = Review
-handleArgs ["status"] = Status
-handleArgs _args = undefined
-
-handleCommand :: Command -> Connection -> IO ()
-handleCommand (AddDrill filePath) conn = drillFromFile filePath conn
-handleCommand Review conn = review conn
-handleCommand Status conn = status conn
+handleCommand :: UI.Command -> Connection -> IO ()
+handleCommand (UI.AddDrill filePath) conn = drillFromFile filePath conn
+handleCommand UI.Review conn = review conn
+handleCommand UI.Status conn = status conn
 
 drillFromFile :: FilePath -> Connection -> IO ()
 drillFromFile filePath conn = do
@@ -84,7 +75,7 @@ review conn = do
               let filename = "/tmp/" <> (fileName drill')
               TIO.writeFile filename (body drill')
               callCommand $ vimCommand filename
-              newScore <- getScore
+              newScore <- UI.getScore
               currentDay <- getCurrentDay
               let newGrade' = applySM2Grade grade' (SM2.applyScore (fromIntegral newScore) (toSM2Grade grade'))
               let newGrade = newGrade' { lastReviewed = currentDay }
@@ -106,36 +97,6 @@ toSM2Grade grade =
 applySM2Grade :: Grade -> SM2.Grade -> Grade
 applySM2Grade grade (SM2.Grade streak score interval) =
     grade { streak = streak, score = score, interval = interval}
-
-getScore :: IO Int
-getScore = do
-    putStrLn "how did you do? score from 0 - 5, ? for help"
-    line <- getLine
-    case line of
-        "?" -> do
-            showHelpText
-            getScore
-        _line -> do
-            let maybeScore = readMaybe line :: Maybe Int
-            handleScore maybeScore
-    where handleScore :: Maybe Int -> IO Int
-          handleScore (Just score) | validScore score = return score
-          handleScore _invalidScore = do
-              putStrLn "Score not recognized."
-              getScore
-          validScore score = score >= 0 && score <= 5
-
-showHelpText :: IO ()
-showHelpText = TIO.putStrLn helpText
-    where helpText =
-             "SCORING\n \
-             \- 0 Total blackout, complete failure to recall the information.\n \
-             \- 1 Incorrect response, but upon seeing the correct answer it felt familiar.\n \
-             \- 2 Incorrect response, but upon seeing the correct answer it seemed easy to remember.\n \
-             \- 3 Correct response, but required significant effort to recall.\n \
-             \- 4 Correct response, after some hesitation.\n \
-             \- 5 Correct response with perfect recall."
-
 
 getCurrentDay :: IO Day
 getCurrentDay = do
@@ -159,6 +120,6 @@ status conn = do
 main :: IO ()
 main = do
     args <- getArgs
-    let command = handleArgs args
+    let command = UI.handleArgs args
     withConnection "vim-gym.db" $ \conn -> do
         handleCommand command conn
--- src/UI.hs
+++ src/UI.hs
@@ -1,6 +1,7 @@
-module UI (getContinue) where
+module UI (getContinue, getScore, Command(..), handleArgs) where
 import Data.Text (Text, toLower)
 import qualified Data.Text.IO as TIO
+import Text.Read (readMaybe)
 
 getContinue :: IO Bool
 getContinue = do
@@ -21,3 +22,40 @@ parseContinue input = parseContinue' cleanInput
           parseContinue' _ = Nothing
 
 
+getScore :: IO Int
+getScore = do
+    putStrLn "how did you do? score from 0 - 5, ? for help"
+    line <- getLine
+    case line of
+        "?" -> do
+            showHelpText
+            getScore
+        _line -> do
+            let maybeScore = readMaybe line :: Maybe Int
+            handleScore maybeScore
+    where handleScore :: Maybe Int -> IO Int
+          handleScore (Just score) | validScore score = return score
+          handleScore _invalidScore = do
+              putStrLn "Score not recognized."
+              getScore
+          validScore score = score >= 0 && score <= 5
+
+showHelpText :: IO ()
+showHelpText = TIO.putStrLn helpText
+    where helpText =
+             "SCORING\n \
+             \- 0 Total blackout, complete failure to recall the information.\n \
+             \- 1 Incorrect response, but upon seeing the correct answer it felt familiar.\n \
+             \- 2 Incorrect response, but upon seeing the correct answer it seemed easy to remember.\n \
+             \- 3 Correct response, but required significant effort to recall.\n \
+             \- 4 Correct response, after some hesitation.\n \
+             \- 5 Correct response with perfect recall."
+
+
+data Command = AddDrill { path :: FilePath } | Review | Status deriving (Show)
+
+handleArgs :: [String] -> Command
+handleArgs ["add", filePath] = AddDrill { path = filePath }
+handleArgs ["review"] = Review
+handleArgs ["status"] = Status
+handleArgs _args = undefined
