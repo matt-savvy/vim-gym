7a7bd76aadb78025c155357104077448285505e8
Matt Savoia <matt@1-800-rad-dude.com>
2023-10-08 14:55:32 -0400

    Update Command, fix partial field

--- src/UI.hs
+++ src/UI.hs
@@ -57,10 +57,10 @@ showHelpText = TIO.putStrLn helpText
 showStatus :: Int -> IO ()
 showStatus n = TIO.putStrLn $ "Drills due for review: " <> (pack . show) n
 
-data Command = AddDrill {path :: FilePath} | Review | Status deriving (Show)
+data Command = AddDrill FilePath | Review | Status deriving (Show)
 
 handleArgs :: [String] -> Command
-handleArgs ["add", filePath] = AddDrill {path = filePath}
+handleArgs ["add", filePath] = AddDrill filePath
 handleArgs ["review"] = Review
 handleArgs ["status"] = Status
 handleArgs _args = undefined
