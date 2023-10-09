b46e1d8f406873615f68817525021a1106efaab2
Matt Savoia <matt@1-800-rad-dude.com>
2023-10-09 10:49:13 -0400

    Update init_db, add files

--- init_db.sql
+++ init_db.sql
@@ -11,3 +11,11 @@ CREATE TABLE grades (
     interval INTEGER,
     last_reviewed TEXT
 );
+
+CREATE TABLE files (
+    id INTEGER PRIMARY KEY,
+    filename TEXT,
+    body TEXT,
+    drill_id INTEGER,
+    FOREIGN KEY(drill_id) REFERENCES drills(id)
+);
