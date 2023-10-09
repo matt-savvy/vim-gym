2edda98986edcf108bc0ada6eda5b86cafd5e561
Matt Savoia <matt@1-800-rad-dude.com>
2023-10-09 10:45:43 -0400

    Refactor init_db, format

--- init_db.sql
+++ init_db.sql
@@ -1,2 +1,13 @@
-CREATE TABLE drills (id INTEGER PRIMARY KEY, filename TEXT, body TEXT);
-CREATE TABLE grades (id INTEGER PRIMARY KEY, drill_id INTEGER, streak INTEGER, score REAL, interval INTEGER, last_reviewed TEXT);
+CREATE TABLE drills (
+    id INTEGER PRIMARY KEY,
+    filename TEXT, body TEXT
+);
+
+CREATE TABLE grades (
+    id INTEGER PRIMARY KEY,
+    drill_id INTEGER,
+    streak INTEGER,
+    score REAL,
+    interval INTEGER,
+    last_reviewed TEXT
+);
